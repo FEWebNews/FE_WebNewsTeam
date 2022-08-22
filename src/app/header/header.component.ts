b3912b874fa90b5d50d4a12c5e4ea4d063152282
ngthekiet <19130112@st.hcmuaf.edu.vn>
2022-08-22 10:41:32 +0700

    last update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { News } from '../model/news.model';
-import { AuthService } from '../services/auth.service';
-import { RssService } from '../services/rss.service';
+import {Component, OnInit} from '@angular/core';
+import {News} from '../model/news.model';
+import {AuthService} from '../services/auth.service';
+import {RssService} from '../services/rss.service';
 
 @Component({
   selector: 'app-header',
@@ -9,27 +9,31 @@ import { RssService } from '../services/rss.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  searchTerm : any
+  searchTerm: any
   dataRSS: News [] = [];
-  public user : any;
-  constructor(private rss: RssService , private auth :AuthService) { }
+  public user: any;
+  date: any;
+
+  constructor(private rss: RssService, private auth: AuthService) {
+  }
 
   ngOnInit(): void {
-
-
     this.rss.getItem("home").subscribe((res) => {
       this.dataRSS = res;
     })
-    this.auth.auth.user.subscribe(user=>{
-      this.user = user;})
+    this.auth.auth.user.subscribe(user => {
+      this.user = user;
+    })
+    this.date = Date.now();
+  }
 
-}
-search(event :any){
-  this.searchTerm = event.target.value
-  console.log(this.searchTerm)
-  this.rss.search.next(this.searchTerm)
-}
-signOut(){
-  this.auth.auth.signOut()
-}
+  search(event: any) {
+    this.searchTerm = event.target.value
+    console.log(this.searchTerm)
+    this.rss.search.next(this.searchTerm)
+  }
+
+  signOut() {
+    this.auth.auth.signOut()
+  }
 }
